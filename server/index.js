c64e48c6b6487fdcb83632467b67c72790cab6e1
sarthu210 <sarthu102@gmail.com>
2025-07-25 15:45:42 +0530

    Made compatible to run for vercel

--- server/index.js
+++ server/index.js
@@ -1,30 +1,31 @@
 import express from 'express'
 import cors from 'cors'
-import env from "dotenv"
-import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import { v4 as uuidv4 } from 'uuid'
-import connection from "./db/connection.js"
-import UserRoutes from "./routes/UserRoutes.js"
+import connection from './db/connection.js'
+import UserRoutes from './routes/UserRoutes.js'
+import serverless from 'serverless-http'
 
-env.config();
-connection();
+dotenv.config()
+connection()
 
 const app = express()
-const PORT = 5000
 
 app.use(express.json())
-app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173', // React app
-  credentials: true
-}));
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // change to your frontend URL on prod
+    credentials: true,
+  })
+)
 
-app.use('/api', UserRoutes);
+app.use('/api', UserRoutes)
 
-app.get("/" , async(req, res) => {{
-  res.status(200).json({"message" : "server is runnig"});
-}})
-
-app.listen(PORT, () => {
-  console.log(`✅ Mock Verifier backend running at: http://localhost:${PORT}`)
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'server is running' })
 })
+
+export const handler = serverless(app)
+export default handler
